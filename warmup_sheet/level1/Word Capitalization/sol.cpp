432cae7d9441fad66bd226f9c351ced19c98d9ed
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-21 15:14:43 +0200

    Word

---
+++ warmup_sheet/level1/Word Capitalization/sol.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+    string s; 
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s;
+
+}
+int main()
+{
+    solution();
+
+    return 0;
+}
