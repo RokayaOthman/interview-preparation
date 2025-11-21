39d2ce9fd0247f3727eed9c63f80a1186fb9def6
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-21 19:23:35 +0200

    Magnets solved

--- warmup_sheet/level1/Magnets/sol.cpp
+++ warmup_sheet/level1/Magnets/sol.cpp
@@ -7,28 +7,27 @@ void solution()
     cin >> n;
     string arr[n];
     int result = 1;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n ; i++)
     {
         cin >> arr[i];
     }
-    if (n == 1)
-        result = 1;
+    
     if (n > 1)
     {
         for (int i = 0; i < n - 1; i++)
-    { 
-        // If adjacent magnets have same orientation, they repel (new group)
-        // If they have different orientations, they attract (same group)
-        if (arr[i] != arr[i + 1])
-        {
-            // They attract, stay in same group
-        }
-        else
-        {
-            // They repel, start new group
-            result++;
-        }
-    }
+                { 
+                    // If adjacent magnets have same orientation, they repel (new group)
+                    // If they have different orientations, they attract (same group)
+                    if (arr[i] != arr[i + 1])
+                        {
+                            result++;
+                        }
+                    else
+                        {
+                            // They repel, start new group
+                        continue;
+                        }
+                }
         
     }
 
